fa1291c2e19a518dfc189a1a22f2a6a8bb81e69a
Dream331 <u62042380120@uru.ac.th>
2023-11-10 20:52:21 +0700

    update services

--- app/Services/page.tsx
+++ app/Services/page.tsx
@@ -1,16 +1,15 @@
-// tsrafce
 import React from 'react'
+import ServicesPage from '../components/ScrollReact/ServicesPage'
 
 type Props = {}
 
 const Services = (props: Props) => {
     return (
         <div className='mb-[200px]'>
-            <div className='flex'>
-<div>
-    {/* <Filter/> */}
-</div>
-            Services
+            <div className='flex justify-center'>
+                <div style={{ marginTop: '50px' }}>
+                    <ServicesPage />
+                </div>
             </div>
         </div>
     )
