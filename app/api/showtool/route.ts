47bdfe16c331af1f03ac2fe30155f3c98b0e0de7
Dream331 <u62042380120@uru.ac.th>
2023-11-23 14:42:35 +0700

    allroom api

--- app/api/showtool/route.ts
+++ app/api/showtool/route.ts
@@ -4,14 +4,14 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
     try {
-        const activeTools = await prisma.tools.findMany({
+        const allactiveTools = await prisma.tools.findMany({
             where: {
                 active: true,
             },
         });
-        return NextResponse.json(activeTools);
+        return NextResponse.json(allactiveTools);
     } catch (error) {
         console.error('Prisma Client Error:', error);
-        throw error; // rethrow the error to let Next.js handle it
+        return NextResponse.error()
     }
 }
